ff6ebd12e1525003f59e59068a4fe921520f8842
AkshitSaini <akshit.saini@u-next.com>
2026-03-10 12:26:28 +0530

    docs added

--- ingestion.py
+++ ingestion.py
@@ -2,5 +2,20 @@ import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_chroma import Chroma
+from langchain_chroma import Chroma #vector database
 from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    print("Loading documents...")
+    # Load documents from the "data" directory
+    # Chunk the files into smaller pieces
+    # Create embeddings for the chunks
+    # Store the embeddings in a vector database
+    
+
+
+if __name__ == "__main__":
+    main()
+
